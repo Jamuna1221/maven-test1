c0fbe66a9b7086b73563fb83c15e6b3bc59adf10
Jamuna Rani <jamunaranic1221@gmail.com>
2026-09-21 10:25:33 +0530

    JAMUNAR0001 - Wrote the code for printing numbers from 1 to 10

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,9 @@
+package com.example.javamavenjunithelloworld;
+
+public class Utility {
+    public static void jamuna() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
